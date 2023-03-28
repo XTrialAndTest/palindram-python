f866455adb99e18da9156853c1f72d629cc7995e
Xtrialandtest <1trialandtest@gmail.com>
2023-03-28 14:55:56 +0300

    RPS added

---
+++ RockPaperScissors/RPS.py
@@ -0,0 +1,98 @@
+import  random
+
+choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+all=['paper','scissors','rock',]
+
+
+
+def randomChoicer():
+    randomize=random.randint(0, len(all)-1)
+    randomChoice=all[randomize]
+    return randomChoice
+
+
+theRandom=randomChoicer()
+
+while True:
+    
+    if choice not in all:
+        choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+    else: 
+        if(choice == theRandom):
+
+            print('this is a tie!', choice,theRandom)
+            choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+            theRandom=randomChoicer()
+            # again=input('do you want to play again? (yes or no): ').lower()
+            # if again=='yes':
+            #     choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+            #     theRandom= randomChoicer()
+            # else: 
+            #     break
+        elif(choice =='rock' ):
+            if(theRandom=='scissors'):
+                print('you win!', theRandom)
+                # choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+                # theRandom=randomChoicer()
+                again=input('do you want to play again? (yes or no): ').lower()
+                if again=='yes':
+                    choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+                    theRandom= randomChoicer()
+                else: 
+                    break
+            elif(theRandom=='paper'):
+                print('you lose!', theRandom)
+                choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+                theRandom=randomChoicer()
+
+
+
+
+
+
+
+        elif(choice =='paper' ):
+            if(theRandom=='rock'):
+                print('you win!', theRandom)
+                # choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+                # theRandom=randomChoicer()
+                again=input('do you want to play again? (yes or no): ').lower()
+                if again=='yes':
+                    choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+                    theRandom= randomChoicer()
+                else: 
+                    break
+            elif(theRandom=='scissors'):
+                print('you lose!', theRandom)
+                choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+                theRandom=randomChoicer()
+
+
+
+
+
+        elif(choice =='scissors' ):
+            if(theRandom=='paper'):
+                print('you win!', theRandom)
+                # choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+                # theRandom=randomChoicer()
+                again=input('do you want to play again? (yes or no): ').lower()
+                if again=='yes':
+                    choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+                    theRandom= randomChoicer()
+                else: 
+                    break
+            elif(theRandom=='rock'):
+                print('you lose!', theRandom)
+                choice=input('please choose one of the following: Rock,Paper, Scissors: ').lower()
+                theRandom=randomChoicer()
+
+
+          
+         
+              
+        else:
+            print('the game is over!',choice,theRandom)
+            break
+
+
